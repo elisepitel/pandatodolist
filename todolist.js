e206a090750be76f188eb32dab64b80a447bc073
Elise <pitel.elise@gmail.com>
2020-02-12 22:47:50 +0100

    add a delete button

--- todolist.js
+++ todolist.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
 const form = document.getElementById('submitTask');
 const input = form.querySelector('input');
 
+
     form.addEventListener ('submit', (e) => {
         e.preventDefault();
         const text = input.value;
@@ -10,9 +11,21 @@ const input = form.querySelector('input');
 
         const ul = document.getElementById('toDoList');
         const li = document.createElement('li');
-
         li.textContent = text;
+
+        let deleteButton = document.createElement('button');
+        deleteButton.textContent = 'delete';
+
         ul.appendChild(li);
+        li.appendChild(deleteButton);
+
+        deleteButton.addEventListener ('click', (e) => {
+            ul.removeChild(li);
+        });
     });
 
+
 });
+
+
+
