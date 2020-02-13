9fb3b56933b4d3815c2ffd2b1c4f10cbfa202264
Elise <pitel.elise@gmail.com>
2020-02-13 22:17:50 +0100

    add an edit button

--- todolist.js
+++ todolist.js
@@ -13,15 +13,29 @@ const input = form.querySelector('input');
         const li = document.createElement('li');
         li.textContent = text;
 
-        let deleteButton = document.createElement('button');
-        deleteButton.textContent = 'delete';
 
         ul.appendChild(li);
+
+        let deleteButton = document.createElement('button');
+        deleteButton.textContent = 'delete';
         li.appendChild(deleteButton);
 
         deleteButton.addEventListener ('click', (e) => {
             ul.removeChild(li);
+
         });
+        
+        let editButton = document.createElement('button');
+        editButton.textContent = 'edit';
+        li.appendChild(editButton);
+
+        editButton.addEventListener ('click', (e) => {
+            let editedTask = document.createElement('input');
+            li.prepend(editedTask);
+            li.childNodes[1].nodeValue = '';
+            editButton.textContent = 'save';
+         });
+
     });
 
 
