8a424804d0e868c94e32217c9cdb44299bcfad7a
Elise <pitel.elise@gmail.com>
2020-02-14 22:19:00 +0100

    add save button

--- todolist.js
+++ todolist.js
@@ -6,14 +6,13 @@ const input = form.querySelector('input');
 
     form.addEventListener ('submit', (e) => {
         e.preventDefault();
-        const text = input.value;
+        const ul = document.getElementById('toDoList');
+        const span = document.createElement('span');
+        const li = document.createElement('li');
+        span.innerHTML = input.value;
         input.value = '';
 
-        const ul = document.getElementById('toDoList');
-        const li = document.createElement('li');
-        li.textContent = text;
-
-
+        li.appendChild(span);
         ul.appendChild(li);
 
         let deleteButton = document.createElement('button');
@@ -22,20 +21,35 @@ const input = form.querySelector('input');
 
         deleteButton.addEventListener ('click', (e) => {
             ul.removeChild(li);
-
         });
         
-        let editButton = document.createElement('button');
+        const editButton = document.createElement('button');
         editButton.textContent = 'edit';
+
+        const saveButton = document.createElement('button');
+        saveButton.textContent = 'save';
+        
+        var editedTask = document.createElement('input');
+        var editedTask = document.createElement('input');
+
         li.appendChild(editButton);
 
-        editButton.addEventListener ('click', (e) => {
-            let editedTask = document.createElement('input');
+        editButton.addEventListener ('click', (e) => { 
+            editedTask.value = span.textContent;
+            li.removeChild(span);
+            li.removeChild(editButton);
             li.prepend(editedTask);
-            li.childNodes[1].nodeValue = '';
-            editButton.textContent = 'save';
+            li.childNodes[1].nodeValue = editedTask.value;
+            li.appendChild(saveButton);
          });
 
+         saveButton.addEventListener ('click', (e) => {
+            span.textContent = editedTask.value;
+            li.removeChild(editedTask);
+            li.removeChild(saveButton);
+            li.prepend(span);
+            li.appendChild(editButton);
+         });
     });
 
 
